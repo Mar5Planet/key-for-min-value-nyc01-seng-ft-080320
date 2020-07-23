5c3d17f82e3b2eb34860ad94488218b16a48bbf2
Mar Mustafa <amustaf@email.arizona.edu>
2020-07-23 16:19:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < limit
       limit = value
-      p name_hash.key(limit)
     end
+    p name_hash.key(limit)
 end
 
 end
